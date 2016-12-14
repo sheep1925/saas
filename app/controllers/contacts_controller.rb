28d99b98750c4c868b2ce615063938b85abda9b3
Matthias Scharfen <Matthias.Scharfen25web.de>
2016-12-14 10:36:50 +0000

    Add creat action to safe data

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,20 @@
 class ContactsController < ApplicationController
     def new
+        @contact = Contact.new
     end
+    
+    def create 
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            redirect_to new_contact_path, notice: "Anfrage gesendet" 
+        else
+            redirect_to new_contact_path, notice: "Es konnte keine Anfrage gesendet werden"
+         end
+    end
+
+private
+    def contact_params
+        params.require(:contact).permit(:name, :email, :comments)
+    end
+    
 end
